6a5129aeb4b0e88e13a028948bf64559fefa5c91
Chris Lafortune <chris.lafortune@gmail.com>
2021-12-11 12:23:57 -0500

    create, read, delete paths completed

--- controllers/collected.js
+++ controllers/collected.js
@@ -2,13 +2,16 @@
 
 const express = require('express');
 const collectedRouter = express.Router();
-const axios = require ('axios');
+const axios = require('axios');
+
 
 // Create a router object
 
 const Title = require('../models/titles');
 
 const BASE_URL = 'https://api.jikan.moe/v3/search/anime?q='
+let animeInfo;
+let foundTitle;
 
 // List router actions
 
@@ -17,25 +20,73 @@ const BASE_URL = 'https://api.jikan.moe/v3/search/anime?q='
 // HOME (SEARCH TITLES)
 
 // INDEX
-
-collectedRouter.get('/', function (req, res, next){
-    axios.get(BASE_URL + 'toradora')
-.then(response => {
-    // console.log(response.data.results);
-    res.render('home.ejs', {results: response.data.results});
-})
+collectedRouter.get('/', function (req, res, next) {
+    res.render('home.ejs');
 })
 
+collectedRouter.get('/search', function (req, res, next) {
+    // console.log(req.query.search);
+    axios.get(BASE_URL + req.query.search)
+        .then(response => {
+            animeInfo = response.data.results;
+            res.render('results.ejs', {
+                results: animeInfo
+            });
+        })
+
+})
+
+collectedRouter.get('/profile', function (req, res) {
+    Title.find({}, function (error, allTitles) {
+        res.render('profile.ejs', {
+            titles: allTitles
+        })
+    })
+})
+
+
+collectedRouter.delete('/profile/:id', function (req, res) {
+    Title.findByIdAndRemove(req.params.id, function (error, deletedTitle) {
+        res.redirect('/profile')
+    })
+})
+
+collectedRouter.get('/profile/:id', function (req, res){
+    Title.findById(req.params.id, function (error, foundTitle){
+        res.render('show.ejs', {
+            title: foundTitle
+        })
+    })
+})
 
 
 
-// collectedRouter.post('/', function (req, res){
 
-// })
+collectedRouter.get('/:id', function (req, res) {
+    foundTitle = animeInfo.find(e => {
+        return e.title === req.params.id;
+    })
+    res.render('info.ejs', {
+        title: foundTitle
+    });
+    // console.log(foundTitle);
+    // res.render('show.ejs', {title: animeInfo}) 
+})
+
+
+collectedRouter.post('/profile', function (req, res) {
+    Title.create(foundTitle, function (error, createdTitle) {
+        res.redirect('/search')
+    });
+})
+
+
 
 // NEW
 
 // DELETE
+
+
 
 // UPDATE
 
--- models/titles.js
+++ models/titles.js
@@ -3,9 +3,35 @@ const Schema = mongoose.Schema;
 
 // Define the schema
 
-const titleSchema = new Schema ({
+const reviewSchema = new Schema({
+    text: String,
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        default: 5
+    }
+  }, { timestamps: true });
 
-})
+const titleSchema = new Schema ({
+    mal_id: Number,
+    url: String,
+    image_url: String,
+    title: String,
+    airing: Boolean,
+    synopsis: String,
+    type: String,
+    episodes: Number,
+    score: Number,
+    start_date: String,
+    end_date: String,
+    members: Number,
+    rated: String,
+    reviewed: {type: Boolean, default: false},
+    epsfinished: {type: Number, default: 0},
+    reviews: [reviewSchema]
+}, {timestamps: true})
+
 
 // Export the model so it can be accessed.
 
